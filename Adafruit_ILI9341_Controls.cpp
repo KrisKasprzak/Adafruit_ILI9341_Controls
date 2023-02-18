746bb1ab4701e0a4664be0bda5b8b473707a4681
Kris Kasprzak <kris.kasprzak@yahoo.com>
2023-02-18 12:26:39 -0600

    Update Adafruit_ILI9341_Controls.cpp

--- Adafruit_ILI9341_Controls.cpp
+++ Adafruit_ILI9341_Controls.cpp
@@ -29,6 +29,8 @@
 	5.0		11/2020			kasprzak			modified sliders, option and check to return true/false if pressed, and actual value stored in value property
 	5.1		11/2020			kasprzak			added automatic "blank out" old handle support insided draw() method in sliderH and SliderV (really needed when a slide is redrawn based on input other than a finger slide (encoder)
 	5.2		1/2023			kasprzak			added icons for buttons, better text centering
+	6.0		2/2023			kasprzak			added mono color icons for buttons
+		
 */
 
 
